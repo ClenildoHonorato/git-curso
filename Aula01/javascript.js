889e172a7c0f437a4b862dcc37b697e43a98ed38
ClenildoHonorato <clenildohonorato@gmail.com>
2018-04-04 13:00:02 -0300

    Todos os arquivos

---
+++ Aula01/javascript.js
@@ -0,0 +1,15 @@
+/*var foto = document.querySelector("#foto");
+
+foto.addEventListener("click", function () {
+    alert("Teste foto");
+});
+
+var botao = document.querySelector(".teste");
+
+botao.addEventListener("click",function(){
+    alert("Teste Botao");
+});*/
+
+function testeimg(){
+    document.querySelector('#foto').src  = "E:\CLENILDO\DesenvolvimentoWeb\fototeste1.jpg";
+}
