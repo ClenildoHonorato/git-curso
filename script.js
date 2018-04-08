138be4952b2313518f3c0cbcbf44ceee9ed0230e
Daniel Lopes <daniel@trilogo.com.br>
2018-04-08 11:33:53 -0300

    Novo Pacote

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
-document.getElementById("teste").addEventListener('click', function(){
-    document.getElementById("foto").src="imagem2.jpg";
+// document.getElementById("teste").addEventListener('click', function(){
+//     document.getElementById("foto").src="imagem2.jpg";
+// });
+document.getElementById("log").addEventListener("submit", function(){
+    console.log("getElementById(log)");
+
+    var usus = document.getElementById("usuario").value;
+    var senha = document.getElementById("senha").value;
+
+    console.log('usuario ', usus);
+    console.log('senha ', senha)
+
+     if(usus == "Clenildo" && senha == 123){
+         alert("Login efetuado com sucesso.")
+     }else{
+         alert("Usuario não cadastrado")
+     }
+     
 });
